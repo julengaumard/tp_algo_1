56a53676f98451cb8f32927d38e1331491ad8b77
Ale <alessandrovperez@gmail.com>
2023-11-16 16:16:42 -0300

    ALGORITMO DE MENSAJES (LA BASE)

--- mensajes.py
+++ mensajes.py
@@ -1,3 +1,19 @@
+from usuarios import buscar_usuario
+from cifrados import cifrar_atbash, cifrar_cesar
 
+def enviar_mensaje(destinatario, remitente, cifrado, clave, mensaje_cifrado):
 
+    if buscar_usuario(destinatario) == destinatario:
+    
+        with open('mensajes.csv', 'a') as mensajes:
+            
+            if cifrar_cesar(cifrado) == cifrado:
+            
+                mensaje = destinatario + "," + remitente + "," + cifrado+clave + "," + mensaje_cifrado + "\n" 
+            
+            elif cifrar_atbash(cifrado) == cifrado:
+                
+                mensaje = destinatario + "," + remitente + "," + cifrado + "," + mensaje_cifrado + "\n" 
+        
+        mensajes.write(mensaje)
 
