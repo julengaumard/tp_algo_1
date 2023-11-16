3cfdcb05cb09c82aabb5cd420235b7a8314aedcf
paladork1 <juanpidominguez03@gmail.com>
2023-11-16 17:45:33 -0300

    Arreglo de codigo

--- mensajes.py
+++ mensajes.py
@@ -4,22 +4,25 @@ from tkinter import messagebox
 
 def enviar_mensaje(destinatario, remitente, cifrado, clave, mensaje_cifrado):
 
-    if buscar_usuario(destinatario) == destinatario:
-    
-        with open('mensajes.csv', 'a') as mensajes:
-            
+    usuario = buscar_usuario(destinatario)
+
+    if not usuario:
+        
+        with open('mensajes.csv', 'a') as ar_mensajes:
+                    
             if cifrado == 3:
                 cifrado = "C"
                 clave = str(clave)
 
                 mensaje = destinatario + "," + remitente + "," + cifrado+clave + "," + mensaje_cifrado + "\n" 
-            
+                    
             elif cifrado == 4:
                 cifrado = "A"
-                
+                        
                 mensaje = destinatario + "," + remitente + "," + cifrado + "," + mensaje_cifrado + "\n" 
-        
-        mensajes.write(mensaje)
+                
+            ar_mensajes.write(mensaje)
 
     else:
         messagebox.showerror("Datos incorrentos","El usuario no existe")
+
