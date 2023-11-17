a465caca2a5a63c9edd16dcf1925a93af11298b0
Ale <alessandrovperez@gmail.com>
2023-11-17 14:50:56 -0300

    se le agrego el mensaje genereal *

--- mensajes.py
+++ mensajes.py
@@ -4,13 +4,7 @@ from tkinter import messagebox
 
 def enviar_mensaje(destinatario, remitente, cifrado, clave, mensaje_cifrado):
 
-    usuario = buscar_usuario(destinatario)
-
-    if not usuario:
-
-        messagebox.showerror("Datos incorrentos","El usuario no existe")
-
-    else:
+    if destinatario == '*':
         with open('mensajes.csv', 'a') as ar_mensajes:
                     
             if cifrado == 3:
@@ -26,6 +20,31 @@ def enviar_mensaje(destinatario, remitente, cifrado, clave, mensaje_cifrado):
                         
                 mensaje = destinatario + "," + remitente + "," + cifrado + "," + mensaje_cifrado + "\n" 
                 messagebox.showinfo("Éxito", "El mensaje se envió correctamente")
-                
+        
             ar_mensajes.write(mensaje)
+
+    else:
+        usuario = buscar_usuario(destinatario)
+
+        if not usuario:
+            messagebox.showerror("Datos incorrentos","El usuario no existe")
+
+        else:
+            with open('mensajes.csv', 'a') as ar_mensajes:
+                    
+                if cifrado == 3:
+                    cifrado = "C"
+                    clave = str(clave)
+
+                    mensaje = destinatario + "," + remitente + "," + cifrado+clave + "," + mensaje_cifrado + "\n" 
+                    messagebox.showinfo("Éxito", "El mensaje se envió correctamente")
+                
+                    
+                elif cifrado == 4:
+                    cifrado = "A"
+                        
+                    mensaje = destinatario + "," + remitente + "," + cifrado + "," + mensaje_cifrado + "\n" 
+                    messagebox.showinfo("Éxito", "El mensaje se envió correctamente")
+                
+                ar_mensajes.write(mensaje)
 
