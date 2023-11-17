50ed47e32420750dc4a25ab573874d7e9eb2dccb
Ale <alessandrovperez@gmail.com>
2023-11-17 17:11:18 -0300

    se creo la funcion de mostra_mensajes_ordenados

--- consultar_mensajes.py
+++ consultar_mensajes.py
@@ -27,16 +27,53 @@ def buscar_mensajes():
             
             
             if usuario == destinatario:
-                mensaje_privado = remitente + ':' + mensaje_descifrado
+                mensaje_privado = remitente + ',' + ':' + ','+ mensaje_descifrado
                 ar_mensaje_privado.write(mensaje_privado)
             
             
             elif usuario == '*':
-                mensaje_general = '*'+ remitente + ':' + mensaje_descifrado
+                mensaje_general = '*' + remitente + ',' + ':' + ','+ mensaje_descifrado
                 ar_mensaje_general.write(mensaje_general)
 
             linea_mensajes = leer_linea(ar_mensajes)
-                
+            
     ar_mensaje_general.close()
     ar_mensaje_privado.close()
+
+def mostrar_mensajes_ordenados():
+    # Autor: Alessandro Perez
     
+    ar_mensaje_general = open("mensajes_general.csv")
+    ar_mensaje_privado = open("mensajes_privado.csv")
+    ar_mensajes_totales = open("mensajes_totales", 'w')
+    
+    linea_mensajes_general = leer_linea(ar_mensaje_general)
+    linea_mensaje_privado = leer_linea(ar_mensaje_privado)
+    
+    while linea_mensajes_general:
+        
+        remitente, separador, mensaje_descifrado = linea_mensajes_general
+        mensaje_general = '*' + remitente + ',' + ':' + ','+ mensaje_descifrado
+        
+        ar_mensajes_totales.write(mensaje_general)
+        
+        linea_mensajes_general = leer_linea(ar_mensaje_general)
+    
+    while linea_mensaje_privado:
+        
+        remitente, separador, mensaje_descifrado = linea_mensajes_general
+        mensaje_privado = remitente + ',' + ':' + ','+ mensaje_descifrado
+        
+        ar_mensajes_totales.write(mensaje_privado)
+        
+        linea_mensaje_privado = leer_linea(ar_mensaje_privado)
+    
+    ar_mensaje_general.close()
+    ar_mensaje_privado.close()
+    ar_mensajes_totales.close()
+
+def main():
+    buscar_mensajes()
+    mostrar_mensajes_ordenados()
+
+main()
